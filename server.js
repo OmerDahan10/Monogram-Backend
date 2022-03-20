7b685677ad26919be27640229778a7f8a583d775
Omer Dahan <omer.dahan26699@gmail.com>
2022-03-20 18:16:25 +0200

    asdasd

--- server.js
+++ server.js
@@ -15,18 +15,18 @@ const session = expressSession({
 app.use(express.json())
 app.use(session)
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.resolve(__dirname, 'public')))
-//     app.use(express.static('public'));
-// } else {
-//     const corsOptions = {
-//         origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
-//         credentials: true
-//     }
-//     app.use(cors(corsOptions))
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'public')))
+    // app.use(express.static('public'));
+} else {
+    const corsOptions = {
+        origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
+        credentials: true
+    }
+    app.use(cors(corsOptions))
+}
 
 const postRoutes = require('./api/post/post.routes')
 const userRoutes = require('./api/user/user.routes')
